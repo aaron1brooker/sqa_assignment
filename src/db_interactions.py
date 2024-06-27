b693d4c954fef1fbbd7e49c6faff39aa52942d3a
alexmcd223 <65975621+alexmcd223@users.noreply.github.com>
2024-06-27 12:59:27 +0100

    black

--- src/db_interactions.py
+++ src/db_interactions.py
@@ -1,8 +1,6 @@
 import mysql.connector
 
-connection = mysql.connector.connect(
-    user="root", password="root", host="mysql", port="3306", database="db"
-)
+connection = mysql.connector.connect(user="root", password="root", host="mysql", port="3306", database="db")
 print("Succesfully connected to db")
 
 cursor = connection.cursor()
