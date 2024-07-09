d8d6baa6282490148cc063f1eddd0194edea9630
alexmcd223 <65975621+alexmcd223@users.noreply.github.com>
2024-07-09 12:13:59 +0100

    first test parametrised

--- tests/integration/tests.py
+++ tests/integration/tests.py
@@ -14,6 +14,7 @@ def add_item_to_db(item_id, item_description, item_completed):
 @pytest.fixture(autouse=True)
 def clear_db():
     yield None
+    # test tear down
     connection = mysql.connector.connect(user="root", password="root", host="mysql", port="3306", database="db")
     cursor = connection.cursor()
     query = "DELETE FROM items"
@@ -21,12 +22,20 @@ def clear_db():
     connection.commit()
     cursor.close()
 
-def test_fetch_items():
+@pytest.mark.parametrize("items", [([{"item_id": "SOME_ID", "description": "SOME_DESCRIPTION", "completed": 0}]),
+                                   
+                                   ([{"item_id": "SOME_ID", "description": "SOME_DESCRIPTION", "completed": 0}, 
+                                     {"item_id": "SOME_ID_1", "description": "SOME_DESCRIPTION_1", "completed": 1}]),
+
+                                   ([{"item_id": "SOME_ID", "description": "SOME_DESCRIPTION", "completed": 0}, 
+                                     {"item_id": "SOME_ID_1", "description": "SOME_DESCRIPTION_1", "completed": 1},
+                                     {"item_id": "SOME_ID_2", "description": "SOME_DESCRIPTION_2", "completed": 0}])
+                                  ]
+                        )
+def test_fetch_items_returns_all_items_in_db(items):
     # GIVEN
-    item_id = "SOME_ID"
-    description = "SOME_DESCRIPTION"
-    completed = 0
-    add_item_to_db(item_id, description, completed)
+    for item in items:
+        add_item_to_db(item["item_id"], item["description"], item["completed"])
 
     # WHEN 
     session = requests.Session()
@@ -34,10 +43,11 @@ def test_fetch_items():
     data = {"user_identifier": "1"}
     response = session.post('http://todo_app:5001/fetch-items', headers=headers, data=json.dumps(data))
     response_obj = json.loads(response.json())
-    item = response_obj["items"][0]
+    response_items = response_obj["items"]
 
     # THEN
-    assert len(response_obj["items"]) == 1
-    assert item["item_id"] == item_id
-    assert item["message"] == description
-    assert item["checked"] == completed
+    assert len(response_obj["items"]) == len(items)
+    for input_item, response_item in zip(items, response_items):
+        assert input_item["item_id"] == response_item["item_id"]
+        assert input_item["description"] == response_item["message"]
+        assert input_item["completed"] == response_item["checked"]
